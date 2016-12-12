d5e68a29bf8f8bef651bab63fca71d957ca0b4e6
Andy Mavroudis <andymavroudis@crescentschool.org>
2016-12-11 21:03:12 -0500

    first night of work:
    all images needed for game up
    abstract class for foundation of two games up
    interface for how the back end of the grid is to work up

--- src/GridMania.java
+++ src/GridMania.java
@@ -1,8 +1,83 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
-public class GridMania 
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public abstract class GridMania extends JPanel 
 {
-	public static void main(String[] args) {
+	BufferedImage mainMenu, ticTac, stones, xWin, oWin, oneWin, twoWin; //menus images
+	BufferedImage xPiece, oPiece, stonePiece; //game component images
+	BufferedImage xPlayer, oPlayer, player1, player2; //which player images
+	BufferedImage turn3, turn2, turn1, turn0; // which turn images
+	public GridMania() throws IOException{
+		URL fileURL; //get images all loaded in
+		
+		//menus
+		fileURL = getClass().getResource("main menu.png");
+		mainMenu = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("stonesGame.png");
+		stones = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("tic tac to.png");
+		ticTac = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("play1WIN.png");
+		oneWin = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("play2WIN.png");
+		twoWin = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("playOWIN.png");
+		oWin = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("playXWIN.png");
+		xWin = ImageIO.read(fileURL);
+		
+		//pieces
+		fileURL = getClass().getResource("x.png");
+		xPiece = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("o.png");
+		oPiece = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("Stone.png");
+		stonePiece = ImageIO.read(fileURL);
+		
+		//player id
+		fileURL = getClass().getResource("Player O.png");
+		oPlayer = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("PLayer X.png");
+		xPlayer = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("PLayer 1.png");
+		player1 = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("PLayer 2.png");
+		player2 = ImageIO.read(fileURL);
+		
+		//stones count
+		fileURL = getClass().getResource("TurnsLeft3.png");
+		turn3 = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("TurnsLeft2.png");
+		turn2 = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("TurnsLeft1.png");
+		turn1 = ImageIO.read(fileURL);
+		
+		fileURL = getClass().getResource("TurnsLeft0.png");
+		turn0 = ImageIO.read(fileURL);
 		
 	}
-
+	
+	public void paintComponent(Graphics g){
+		g.drawImage(mainMenu, 0, 0, null);
+	}
+	
+	abstract void run(); 	//how game will run
 }
--- src/ProjectMain.java
+++ src/ProjectMain.java
@@ -1,9 +1,39 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ProjectMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// declare and initialize a JFrame
+				JFrame myFrame = new JFrame();
 
+				// config frame settings
+				myFrame.setLocationRelativeTo(null);
+
+				myFrame.setTitle("Grid Mania");
+				myFrame.setSize(800, 800);
+				myFrame.setResizable(false);
+				myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				// Get the size of the screen
+				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+				int x = (dim.width - myFrame.getSize().width) / 2;
+
+				int y = (dim.height - myFrame.getSize().height) / 2;
+
+				// center it
+				myFrame.setLocation(x, y);
+
+				// make our frame visible
+				myFrame.setVisible(true);
+				
+				
+				
 	}
 
 }
